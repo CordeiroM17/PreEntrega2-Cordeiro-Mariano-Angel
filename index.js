2dd093adc14291605d25fd1f765cd587e4684e31
Dripper <cordeiromariano17@gmail.com>
2022-12-15 17:15:51 -0300

    .

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 let base_de_pacientes = [];
 
-function paciente(nombre, apellido, correo, grupo_sanguineo, factor) {
+function paciente(nombre, apellido, edad, grupo_sanguineo, factor) {
     this.nombre = nombre;
     this.apellido = apellido;
-    this.correo = correo;
+    this.edad = edad;
     this.grupo_sanguineo = grupo_sanguineo;
     this.factor = factor;
 }
@@ -14,7 +14,7 @@ function capturar() {
      
     let nombre_capturado = document.getElementById("nombre").value;
     let apellido_capturado = document.getElementById("apellido").value;
-    let correo_capturado = document.getElementById("correo").value;
+    let edad_capturado = document.getElementById("edad").value;
     let grupo_sanguineo_capturado = document.getElementById("grupo_sanguineo").value;
     let factor_capturado = document.getElementById("factor").value;
 
@@ -26,7 +26,7 @@ function capturar() {
         container_datos.append(datos);
         datos.innerHTML = `<h3> ${nuevo_paciente.nombre}</h3>
                             <h3> ${nuevo_paciente.apellido}</h3>
-                            <h3> ${nuevo_paciente.correo}</h3>
+                            <h3> ${nuevo_paciente.edad}</h3>
                             <h3> ${nuevo_paciente.grupo_sanguineo}</h3>
                             <h3> ${nuevo_paciente.factor}</h3>
                             `;
@@ -39,7 +39,7 @@ function capturar() {
     }
 
     if (nombre_capturado != "" && apellido_capturado != "" && correo_capturado != "") {
-        nuevo_paciente = new paciente(nombre_capturado, apellido_capturado, correo_capturado, grupo_sanguineo_capturado, factor_capturado);
+        nuevo_paciente = new paciente(nombre_capturado, apellido_capturado, edad_capturado, grupo_sanguineo_capturado, factor_capturado);
         console.log(nuevo_paciente);
         agregar_paciente();
         agrega_datos_paciente();
